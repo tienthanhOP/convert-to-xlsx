7e61147dd12ed9148e8c8934eec4b34b9f3dbfbd
thanhpt <thanhpt@scuti.asia>
2022-06-01 22:35:03 +0700

    + download xlsx

--- src/App.js
+++ src/App.js
@@ -1,50 +1,13 @@
 import React, { useState } from "react";
 import "./App.css";
 import { Button, Form, Input, Table, Typography } from "antd";
-import { convertTextToObject } from "./utils";
+
+import { convertTextToObject, downloadXLSX } from "./utils";
 const { Paragraph } = Typography;
 
 const validateMessages = {
   required: "Bạn chưa nhập thông tin đơn hàng!",
 };
-
-const columns = [
-  {
-    title: "Người nhận",
-    dataIndex: "receiver",
-    key: "receiver",
-  },
-  {
-    title: "Địa chỉ",
-    dataIndex: "address",
-    key: "address",
-  },
-  {
-    title: "Số điện thoại",
-    dataIndex: "phone",
-    key: "phone",
-  },
-  {
-    title: "Thỉnh",
-    dataIndex: "buying",
-    key: "buying",
-  },
-  {
-    title: "Phí ship",
-    dataIndex: "freeship",
-    key: "freeship",
-  },
-  {
-    title: "Tổng giá",
-    dataIndex: "price",
-    key: "price",
-  },
-  {
-    title: "Thông tin thanh toán",
-    dataIndex: "paid",
-    key: "paid",
-  },
-];
 
 const App = () => {
   const [orders, setOrders] = useState([]);
@@ -57,28 +20,54 @@ const App = () => {
     }
   };
 
-  const _renderSample = () => {
-    return (
-      <Paragraph className="flex-1 justify-start items-start text-left">
-        <pre>
-          Vậy em chốt đơn của chị nhé
-          <br />
-          Thông tin đơn hàng
-          <br />
-          Người nhận: ...
-          <br />
-          Địa chỉ: ...
-          <br />
-          THỈNH: ...
-          <br />
-          Phí ship bên vận chuyển thu ạ
-          <br />
-          Chị kiểm tra lại giúp em xem thông tin đã chính xác chưa ạ, để em ship
-          sớm cho chị nha
-        </pre>
-      </Paragraph>
-    );
-  };
+  const onDelete = (id) => {
+    setOrders(orders.filter(e => e.id !== id))
+  }
+
+  const columns = [
+    {
+      title: "STT",
+      dataIndex: "index",
+      key: "index",
+      render: (value, item, index) => index + 1
+    },
+    {
+      title: "Mã vận đơn",
+      dataIndex: "freeship",
+      key: "freeship",
+    },
+    {
+      title: "Họ và tên",
+      dataIndex: "receiver",
+      key: "receiver",
+    },
+    {
+      title: "Địa chỉ",
+      dataIndex: "address",
+      key: "address",
+    },
+    {
+      title: "SĐT",
+      dataIndex: "phone",
+      key: "phone",
+    },
+    {
+      title: "Chi tiết",
+      dataIndex: "buying",
+      key: "buying",
+    },
+    {
+      title: "Tiền",
+      dataIndex: "price",
+      key: "price",
+    },
+    {
+      title: "",
+      dataIndex: "delete",
+      key: "delete",
+      render: (value, item) => <a onClick={() => onDelete(item.id)}>Xóa</a>
+    },
+  ];
 
   return (
     <div className="App p-3">
@@ -93,7 +82,7 @@ const App = () => {
                 },
               ]}
             >
-              <Input.TextArea rows={10} />
+              <Input.TextArea rows={8} />
             </Form.Item>
             <Form.Item>
               <Button type="primary" htmlType="submit">
@@ -102,12 +91,11 @@ const App = () => {
             </Form.Item>
           </Form>
         </div>
-        {_renderSample()}
       </div>
-      <Button type="primary" onClick={() => setOrders([])}>
-        Clear
+      <Button className="mb-2" type="primary" onClick={() => downloadXLSX(orders)}>
+        Tải về
       </Button>
-      <Table columns={columns} dataSource={orders} />
+      <Table columns={columns} dataSource={orders} size={100} />
     </div>
   );
 };
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,9 @@
+import { saveAs } from "file-saver";
+import XLSX from 'xlsx-populate/browser/xlsx-populate';
 import {
   ADDRESS_TEXT,
   BUYING_TEXT,
+  PAID,
   PHONE_NUMBER_TEXT,
   RECEIVER_TEXT,
   SHIP,
@@ -8,6 +11,7 @@ import {
 
 export const convertTextToObject = (order) => {
   const result = {};
+  result.id = uuidV4();
   result.receiver = order
     .substring(
       order.indexOf(RECEIVER_TEXT) + RECEIVER_TEXT.length,
@@ -32,9 +36,8 @@ export const convertTextToObject = (order) => {
       order.indexOf(SHIP)
     )
     .trim();
-  result.freeship = order.includes(SHIP) ? "Bên vận chuyển thu" : "Freeship";
-  const equalSign = "=";
   const kCharacter = "k";
+  const equalSign = "=";
   if (result.buying.includes(equalSign)) {
     const txtAfterEqualSign = result.buying.substring(
       result.buying.indexOf(equalSign) + equalSign.length,
@@ -56,6 +59,29 @@ export const convertTextToObject = (order) => {
       }
     }
   }
+  const paid = order.includes(PAID);
+  if (paid) {
+    const txtAfterPaid = order.substring(
+      order.indexOf(PAID) + PAID.length,
+      order.length
+    );
+    let paidNumber = txtAfterPaid
+      .substring(0, txtAfterPaid.indexOf(kCharacter))
+      .trim();
+    if (isNumeric(paidNumber)) {
+      const tmp = parseInt(paidNumber) - parseInt(result.price);
+      const isFreeship = !order.includes(SHIP);
+      if (tmp >= 30) {
+        result.freeship = `đã ck ${paidNumber} cả ship`;
+      } else if (tmp < 0) {
+        result.freeship = `đã ck ${paidNumber} còn ${-tmp}k + ${isFreeship ? 'freeship' : 'phí ship người nhận trả'}`;
+      } else {
+        result.freeship = `đã ck ${paidNumber} + ${isFreeship ? 'freeship' : 'phí ship người nhận trả'}`;
+      }
+    }
+  } else {
+    result.freeship = order.includes(SHIP) ? "Shipcod + phí ship người nhận trả" : "Shipcod + freeship";
+  }
 
   return result;
 };
@@ -63,3 +89,58 @@ export const convertTextToObject = (order) => {
 const isNumeric = (n) => {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
+
+export const uuidV4 = () => {
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+}
+
+const getSheetData = (data, header) => {
+  var fields = Object.keys(data[0]);
+  var sheetData = data.map(function (row) {
+    return fields.map(function (fieldName) {
+      return row[fieldName] ? row[fieldName] : "";
+    });
+  });
+  sheetData.unshift(header);
+  return sheetData;
+}
+
+export const downloadXLSX = async (orders) => {
+  let header = [
+    "STT",
+    "MÃ VẬN ĐƠN",
+    "HỌ VÀ TÊN",
+    "ĐỊA CHỈ",
+    "TỈNH ĐẾN",
+    "SĐT", "GH",
+    "SL",
+    "CHI TIẾT",
+    "NO",
+    "KL",
+    "TẶNG",
+    "TIỀN",
+    "PHÍ SHIP",
+    "NGÀY",
+    "NOTE",
+    "NGƯỜI LÊN ĐƠN",
+    "NGƯỜI CHỐT"
+  ];
+
+  XLSX.fromBlankAsync().then(async (workbook) => {
+    const sheet1 = workbook.sheet(0);
+    const sheetData = getSheetData(orders, header);
+    const totalColumns = sheetData[0].length;
+
+    sheet1.cell("A1").value(sheetData);
+    const range = sheet1.usedRange();
+    const endColumn = String.fromCharCode(64 + totalColumns);
+    sheet1.row(1).style("bold", true);
+    sheet1.range("A1:" + endColumn + "1").style("fill", "FFFF00");
+    range.style("border", true);
+    return workbook.outputAsync().then((res) => {
+      saveAs(res, "file.xlsx");
+    });
+  });
+}
